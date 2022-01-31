2752ed41e137fc9f0d883fbf019970ee8a42ea93
nanderstabel <nanderstabel@gmail.com>
2022-01-31 19:58:34 +0100

    Add tests to curve and inverse_function

--- src/bin/curve.rs
+++ src/bin/curve.rs
@@ -8,10 +8,25 @@ fn map(x: u16, y: u16) -> f64 {
             res |= 1 << (b + 1);
         }
     }
-    res as f64 / u16::MAX as f64
+    res as f64 / u32::MAX as f64
 }
 
 #[allow(dead_code)]
 fn main() {
-    println!("{:#?}", map(13, 27));
+    println!("{:#?}", map(u16::MAX, u16::MAX));
+}
+
+#[cfg(test)]
+mod curve {
+    use super::*;
+
+    #[test]
+    fn assert_equal() {
+        assert_eq!(map(0, 0), 0.0);
+        assert_eq!(map(2, 6), 0.000000010244548323155508);
+        assert_eq!(map(364, 6323), 0.009790322279974428);
+        assert_eq!(map(12345, 1), 0.019531563627424548);
+        assert_eq!(map(314, 2526), 0.0020089102913646286);
+        assert_eq!(map(u16::MAX, u16::MAX), 1.0);
+    }
 }
--- src/bin/inverse_function.rs
+++ src/bin/inverse_function.rs
@@ -1,5 +1,5 @@
 fn reverse_map(n: f64) -> (u16, u16) {
-    let res = (n * u16::MAX as f64) as u32;
+    let res = (n * u32::MAX as f64) as u32;
     let (mut x, mut y) = (0u16, 0u16);
     for b in (0..32).step_by(2) {
         if res & (1 << b) != 0 {
@@ -15,4 +15,19 @@ fn reverse_map(n: f64) -> (u16, u16) {
 #[allow(dead_code)]
 fn main() {
     println!("{:#?}", reverse_map(0.011154345006485085));
+}
+
+#[cfg(test)]
+mod curve {
+    use super::*;
+
+    #[test]
+    fn assert_equal() {
+        assert_eq!(reverse_map(0.0), (0, 0));
+        assert_eq!(reverse_map(0.000000010244548323155508), (2, 6));
+        assert_eq!(reverse_map(0.009790322279974428), (364, 6323));
+        assert_eq!(reverse_map(0.019531563627424548), (12345, 1));
+        assert_eq!(reverse_map(0.0020089102913646286), (314, 2526));
+        assert_eq!(reverse_map(1.0), (u16::MAX, u16::MAX));
+    }
 }
